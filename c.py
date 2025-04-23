e4987e4b3e460b9f5bd4e0b7cf1a4737126d9e73
Rudhhi Shah <rudhhisangitashah@gmail.com>
2025-04-23 15:49:20 +0530

    Initial commit

---
+++ c.py
@@ -0,0 +1,21 @@
+from pyannote.audio import Pipeline
+import torch
+
+# Load pipeline
+pipeline = Pipeline.from_pretrained(
+    "pyannote/speaker-diarization-3.1",
+)
+
+# Send to GPU (optional)
+pipeline.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+# Run on audio file
+diarization = pipeline(r"input_audio\audio_16k_7_clean_output_1.wav")
+
+# Output RTTM format (standard diarization format)
+with open("audio.rttm", "w") as f:
+    diarization.write_rttm(f)
+
+# Print speaker segments
+for turn, _, speaker in diarization.itertracks(yield_label=True):
+    print(f"Speaker {speaker}: {turn.start:.1f}s --> {turn.end:.1f}s")
